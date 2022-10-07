2deea946a5f830f16b5b4e53e59874e252a12777
cnick <cnick@netapp.com>
2022-10-07 16:00:13 -0600

    Save data to speed up everything add FPA

--- nickknows/nfl/views.py
+++ nickknows/nfl/views.py
@@ -4,6 +4,8 @@ from nickknows import app
 import nfl_data_py as nfl
 import pandas as pd
 from IPython.display import HTML
+import os
+import time
 pd.options.mode.chained_assignment = None
 
 @app.route('/NFL')
@@ -12,7 +14,17 @@ def NFL():
 
 @app.route('/NFL/schedule/<week>')
 def schedule(week):
-    schedule = nfl.import_schedules([2022])
+    print()
+    file_path = os.getcwd() + '/nickknows/nfl/data/schedule.csv'
+    if os.path.exists(file_path):
+        if (time.time() - os.path.getmtime(file_path)) > (7 * 24 * 60 * 60):
+            schedule = nfl.import_schedules([2022])
+            schedule.to_csv(file_path)
+        else:
+            schedule = pd.read_csv(file_path, index_col=0)
+    else:
+        schedule = nfl.import_schedules([2022])
+        schedule.to_csv(file_path)
     week_schedule = schedule.loc[schedule['week'] == int(week)]
     url = str('<a href="http://127.0.0.1:5000/NFL/PbP/') + week_schedule['game_id'] + str('">') + week_schedule['game_id'] + str('</a>')
     week_schedule['game_id'] = url
@@ -27,7 +39,16 @@ def schedule(week):
 
 @app.route('/NFL/Roster/<team>/<fullname>')
 def roster(team,fullname):
-    roster_data = nfl.import_rosters([2022])
+    file_path = os.getcwd() + '/nickknows/nfl/data/rosters.csv'
+    if os.path.exists(file_path):
+        if (time.time() - os.path.getmtime(file_path)) > (7 * 24 * 60 * 60):
+            roster_data = nfl.import_rosters([2022])
+            roster_data.to_csv(file_path)
+        else:
+            schedule = pd.read_csv(file_path, index_col=0)
+    else:
+        roster_data = nfl.import_rosters([2022])
+        roster_data.to_csv(file_path)
     team_roster = roster_data.loc[roster_data['team'] == team]
     url = str('<a href="http://127.0.0.1:5000/NFL/Player/') + team_roster['player_name'] + str('">') + team_roster['player_name'] + str('</a>')
     team_roster['player_name'] = url
@@ -42,7 +63,16 @@ def roster(team,fullname):
 
 @app.route('/NFL/PbP/<game>')
 def game_pbp(game):
-    pbp_data = nfl.import_pbp_data([2022])
+    file_path = os.getcwd() + '/nickknows/nfl/data/pbp_data.csv'
+    if os.path.exists(file_path):
+        if (time.time() - os.path.getmtime(file_path)) > (7 * 24 * 60 * 60):
+            pbp_data = nfl.import_pbp_data([2022])
+            pbp_data.to_csv(file_path)
+        else:
+            pbp_data = pd.read_csv(file_path, index_col=0)
+    else:
+        pbp_data = nfl.import_pbp_data([2022])
+        pbp_data.to_csv(file_path)
     game_data = pbp_data.loc[pbp_data['game_id'] == game]
     game_data.rename(columns={'posteam':'Possession','defteam':'Defense','side_of_field':'Field Side','yardline_100':'Distance from EndZone','quarter_seconds_remaining':'Seconds left in Quarter','half_seconds_remaining':'Seconds left in Half','game_seconds_remaining':'Seconds left in Game','drive':'Drive #'}, inplace=True)
     game_data = game_data.style.hide(axis="index")
@@ -54,7 +84,16 @@ def game_pbp(game):
 
 @app.route('/NFL/Player/<name>')
 def player_stats(name):
-    weekly_data = nfl.import_weekly_data([2022])
+    file_path = os.getcwd() + '/nickknows/nfl/data/weekly_data.csv'
+    if os.path.exists(file_path):
+        if (time.time() - os.path.getmtime(file_path)) > (7 * 24 * 60 * 60):
+            weekly_data = nfl.import_weekly_data([2022])
+            weekly_data.to_csv(file_path)
+        else:
+            weekly_data = pd.read_csv(file_path, index_col=0)
+    else:
+        weekly_data = nfl.import_weekly_data([2022])
+        weekly_data.to_csv(file_path)
     player_data = weekly_data.loc[weekly_data['player_display_name'] == name]
     player_data = player_data.style.hide(axis="index")
     player_data = player_data.set_table_attributes({'border-collapse' : 'collapse','border-spacing' : '0px'})
@@ -64,7 +103,16 @@ def player_stats(name):
 
 @app.route('/NFL/Team/<team>/Schedule/<fullname>')
 def team_schedule(team, fullname):
-    schedule = nfl.import_schedules([2022])
+    file_path = os.getcwd() + '/nickknows/nfl/data/schedule.csv'
+    if os.path.exists(file_path):
+        if (time.time() - os.path.getmtime(file_path)) > (7 * 24 * 60 * 60):
+            schedule = nfl.import_schedules([2022])
+            schedule.to_csv(file_path)
+        else:
+            schedule = pd.read_csv(file_path, index_col=0)
+    else:
+        schedule = nfl.import_schedules([2022])
+        schedule.to_csv(file_path)
     url = str('<a href="http://127.0.0.1:5000/NFL/PbP/') + schedule['game_id'] + str('">') + schedule['game_id'] + str('</a>')
     schedule['game_id'] = url
     home_team_schedule = schedule.loc[schedule['home_team'] == team]
@@ -77,7 +125,16 @@ def team_schedule(team, fullname):
 
 @app.route('/NFL/Team/<team>/Results/<fullname>')
 def team_results(team, fullname):
-    schedule = nfl.import_schedules([2022])
+    sched_path = os.getcwd() + '/nickknows/nfl/data/schedule.csv'
+    if os.path.exists(sched_path):
+        if (time.time() - os.path.getmtime(sched_path)) > (7 * 24 * 60 * 60):
+            schedule = nfl.import_schedules([2022])
+            schedule.to_csv(sched_path)
+        else:
+            schedule = pd.read_csv(sched_path, index_col=0)
+    else:
+        schedule = nfl.import_schedules([2022])
+        schedule.to_csv(sched_path)
     url = str('<a href="http://127.0.0.1:5000/NFL/PbP/') + schedule['game_id'] + str('">') + schedule['game_id'] + str('</a>')
     schedule['game_id'] = url
     home_team_schedule = schedule.loc[schedule['home_team'] == team]
@@ -87,5 +144,72 @@ def team_results(team, fullname):
     full_schedule = full_schedule.dropna(subset=['away_score'])
     full_schedule = full_schedule.sort_values(by=['week'])
     full_schedule = full_schedule.style.hide(axis="index").set_table_attributes({'border-collapse' : 'collapse','border-spacing' : '0px'}).set_table_styles([{'selector': 'th', 'props' : 'background-color : gainsboro; color:black; border: 2px solid black;padding : 2.5px;margin : 0 auto; font-size : 12px'}]).set_properties(**{'background-color' : 'gainsboro', 'color' :'black', 'border': '2px solid black','padding' : '2.5px','margin' : '0 auto', 'font-size' : '12px'}).hide(['season','game_type','location','old_game_id','gsis','nfl_detail_id','pfr','pff','espn','away_qb_id','home_qb_id','stadium_id'], axis="columns")
-    
     return render_template('team-schedule.html', team_schedule = full_schedule.to_html(), fullname = fullname)
+
+@app.route('/NFL/Team/<team>/FPA/<fullname>')
+def team_fpa(team, fullname):
+    sched_path = os.getcwd() + '/nickknows/nfl/data/schedule.csv'
+    if os.path.exists(sched_path):
+        if (time.time() - os.path.getmtime(sched_path)) > (7 * 24 * 60 * 60):
+            schedule = nfl.import_schedules([2022])
+            schedule.to_csv(sched_path)
+        else:
+            schedule = pd.read_csv(sched_path, index_col=0)
+    else:
+        schedule = nfl.import_schedules([2022])
+        schedule.to_csv(sched_path)
+    url = str('<a href="http://127.0.0.1:5000/NFL/PbP/') + schedule['game_id'] + str('">') + schedule['game_id'] + str('</a>')
+    schedule['game_id'] = url
+    home_team_schedule = schedule.loc[schedule['home_team'] == team]
+    away_team_schedule = schedule.loc[schedule['away_team'] == team]
+    full_schedule = [home_team_schedule, away_team_schedule]
+    full_schedule = pd.concat(full_schedule)
+    full_schedule = full_schedule.dropna(subset=['away_score'])
+    full_schedule = full_schedule.sort_values(by=['week'])
+    ishome = full_schedule['home_team'].str.contains(team)
+    full_schedule['is_home'] = ishome
+    op_team1 = full_schedule.loc[full_schedule['is_home'] == True, ['away_team', 'week']]
+    op_team2 = full_schedule.loc[full_schedule['is_home'] == False, ['home_team', 'week']]
+    op_team = [op_team1, op_team2]
+    op_team = pd.concat(op_team)
+    op_team["op_team"] = op_team['away_team'].fillna('') + op_team['home_team'].fillna('')
+    op_team = op_team.sort_values(by=['week'])
+    op_team = op_team["op_team"].to_list()
+    weekly_team_data = pd.DataFrame()
+    for team in op_team:
+        week = op_team.index(team) + 1
+        rost_path = os.getcwd() + '/nickknows/nfl/data/rosters.csv'
+        if os.path.exists(rost_path):
+            if (time.time() - os.path.getmtime(rost_path)) > (7 * 24 * 60 * 60):
+                roster_data = nfl.import_rosters([2022])
+                roster_data.to_csv(rost_path)
+            else:
+                roster_data = pd.read_csv(rost_path, index_col=0)
+        else:
+            roster_data = nfl.import_rosters([2022])
+            roster_data.to_csv(rost_path)
+        team_roster = roster_data.loc[roster_data['team'] == team]
+        players = team_roster['player_name'].to_list()
+        for player in players:
+            week_path = os.getcwd() + '/nickknows/nfl/data/weekly_data.csv'
+            if os.path.exists(week_path):
+                if (time.time() - os.path.getmtime(week_path)) > (7 * 24 * 60 * 60):
+                    weekly_data = nfl.import_weekly_data([2022])
+                    weekly_data.to_csv(week_path)
+                else:
+                    weekly_data = pd.read_csv(week_path, index_col=0)
+            else:
+                weekly_data = nfl.import_weekly_data([2022])
+                weekly_data.to_csv(week_path)
+            player_data = weekly_data.loc[weekly_data['player_display_name'] == player]
+            player_data = player_data.loc[player_data['week'] == week]
+            weekly_team_data = [weekly_team_data, player_data]
+            weekly_team_data = pd.concat(weekly_team_data)
+    full_schedule = full_schedule.style.hide(axis="index").set_table_attributes({'border-collapse' : 'collapse','border-spacing' : '0px'}).set_table_styles([{'selector': 'th', 'props' : 'background-color : gainsboro; color:black; border: 2px solid black;padding : 2.5px;margin : 0 auto; font-size : 12px'}]).set_properties(**{'background-color' : 'gainsboro', 'color' :'black', 'border': '2px solid black','padding' : '2.5px','margin' : '0 auto', 'font-size' : '12px'}).hide(['season','game_type','location','old_game_id','gsis','nfl_detail_id','pfr','pff','espn','away_qb_id','home_qb_id','stadium_id'], axis="columns")
+    pass_data = weekly_team_data.loc[weekly_team_data['attempts'] > 0]
+    rush_data = weekly_team_data.loc[weekly_team_data['carries'] > 0]
+    rec_data = weekly_team_data.loc[weekly_team_data['targets'] > 0]
+    pass_data = pass_data.style.hide(axis="index").set_table_attributes({'border-collapse' : 'collapse','border-spacing' : '0px'}).set_table_styles([{'selector': 'th', 'props' : 'background-color : gainsboro; color:black; border: 2px solid black;padding : 2.5px;margin : 0 auto; font-size : 12px'}]).set_properties(**{'background-color' : 'gainsboro', 'color' :'black', 'border': '2px solid black','padding' : '2.5px','margin' : '0 auto', 'font-size' : '12px'}).hide(['player_id','player_name','position_group','headshot_url','season','season_type','carries','rushing_yards','rushing_tds','rushing_fumbles','rushing_fumbles_lost','rushing_first_downs','rushing_epa','rushing_2pt_conversions','receptions','targets','receiving_yards','receiving_tds','receiving_fumbles','receiving_fumbles_lost','receiving_air_yards','receiving_yards_after_catch','receiving_first_downs','receiving_epa','receiving_2pt_conversions','racr','target_share','air_yards_share','wopr','special_teams_tds'], axis="columns")
+    rush_data = rush_data.style.hide(axis="index").set_table_attributes({'border-collapse' : 'collapse','border-spacing' : '0px'}).set_table_styles([{'selector': 'th', 'props' : 'background-color : gainsboro; color:black; border: 2px solid black;padding : 2.5px;margin : 0 auto; font-size : 12px'}]).set_properties(**{'background-color' : 'gainsboro', 'color' :'black', 'border': '2px solid black','padding' : '2.5px','margin' : '0 auto', 'font-size' : '12px'}).hide(['player_id','player_name','position_group','headshot_url','season','season_type','completions','attempts','passing_yards','passing_tds','interceptions','sacks','sack_yards','sack_fumbles','sack_fumbles_lost','passing_air_yards','passing_yards_after_catch','passing_first_downs','passing_epa','passing_2pt_conversions','pacr','dakota','receptions','targets','receiving_yards','receiving_tds','receiving_fumbles','receiving_fumbles_lost','receiving_air_yards','receiving_yards_after_catch','receiving_first_downs','receiving_epa','receiving_2pt_conversions','racr','target_share','air_yards_share','wopr','special_teams_tds'], axis="columns")
+    rec_data = rec_data.style.hide(axis="index").set_table_attributes({'border-collapse' : 'collapse','border-spacing' : '0px'}).set_table_styles([{'selector': 'th', 'props' : 'background-color : gainsboro; color:black; border: 2px solid black;padding : 2.5px;margin : 0 auto; font-size : 12px'}]).set_properties(**{'background-color' : 'gainsboro', 'color' :'black', 'border': '2px solid black','padding' : '2.5px','margin' : '0 auto', 'font-size' : '12px'}).hide(['player_id','player_name','position_group','headshot_url','season','season_type','completions','attempts','passing_yards','passing_tds','interceptions','sacks','sack_yards','sack_fumbles','sack_fumbles_lost','passing_air_yards','passing_yards_after_catch','passing_first_downs','passing_epa','passing_2pt_conversions','pacr','dakota','carries','rushing_yards','rushing_tds','rushing_fumbles','rushing_fumbles_lost','rushing_first_downs','rushing_epa','rushing_2pt_conversions'], axis="columns")
+    return render_template('team-fpa.html', team_fpa = full_schedule.to_html(), fullname = fullname, rush_data=rush_data.to_html(), pass_data = pass_data.to_html(), rec_data = rec_data.to_html())
