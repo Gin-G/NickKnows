f5135756a4f015fede480e3017e7f3b673cc53b1
Nick Cote <NickCo7@gmail.com>
2023-02-16 13:57:35 -0700

    split updates

--- nickknows/nfl/views.py
+++ nickknows/nfl/views.py
@@ -83,11 +83,15 @@ def NFL():
     except:
         return render_template('nfl-home.html')
 
-@app.route('/NFL/update')
-def NFLupdate():
+@app.route('/NFL/update/PBP')
+def NFLupdatePBP():
     file_path = os.getcwd() + '/nickknows/nfl/data/pbp_data.csv'
     pbp_data = nfl.import_pbp_data([2022])
     pbp_data.to_csv(file_path)
+    return redirect(url_for('NFL'))
+
+@app.route('/NFL/update/Rosters')
+def NFLupdateRosters():
     rfile_path = os.getcwd() + '/nickknows/nfl/data/rosters.csv'
     roster_data = nfl.import_rosters([2022])
     roster_data.to_csv(rfile_path)
