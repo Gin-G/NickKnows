839430a272e5681b9fb791056eb605e9579f0393
Gin-G <NickCo7@gmail.com>
2023-03-10 14:55:59 -0700

    Resume compare

--- nickknows/main/views.py
+++ nickknows/main/views.py
@@ -1,4 +1,5 @@
-from flask import redirect, url_for, render_template
+from flask import redirect, url_for, render_template, request
+import json
 from nickknows import app
 
 @app.route('/')
@@ -32,3 +33,92 @@ def guitar():
 @app.route('/skiing')
 def skiing():
     return render_template('skiing.html')
+
+@app.route('/job_parse')
+def job_parse():
+    return render_template('job-parse.html')
+
+@app.route('/job_parse/count')
+def job_parse_count():
+    description = request.args.get('job_desc')
+    job_words = {}
+    omit_words = ['and', 'to', 'of', 'in', 'the', 'with', 'or', 'and/or', 'i.e.', 'etc.', 'as', 'by', 'for', 'on', 'not', 'but', 'is']
+    description = description.split(' ')
+    for words in description:
+        words = words.strip()
+        words = words.replace('/', ' ')
+        words = words.replace('\n', ' ')
+        if len(words) == 0:
+            pass
+        else:
+            words_list = words.split(' ')
+            for word in words_list:
+                word = word.lower()
+                word = word.strip()
+                word = word.replace(',','')
+                word = word.replace("'",'')
+                word = word.replace(':','')
+                word = word.replace('.','')
+                word = word.replace(')','')
+                word = word.replace('(','')
+                if len(word) <= 1:
+                    pass
+                elif word in omit_words:
+                    pass
+                else:
+                    if word not in job_words:
+                        job_words[word] = 1
+                    elif word in job_words:
+                        job_words[word] += 1
+    sorted_words = sorted(job_words.items(), key=lambda kv: kv[1], reverse=True)
+    sorted_words = dict(sorted_words)
+    return render_template('job-parse-count.html', job_count = sorted_words)
+
+@app.route('/job_parse/resume')
+def job_parse_resume():
+    job_count = request.args.get('job_count')
+    job_count = job_count.replace("'",'"')
+    job_count = job_count.replace("’","'")
+    job_count = json.loads(job_count)
+    return render_template('resume-compare.html', job_count = job_count)
+
+@app.route('/job_parse/resume-comp')
+def resume_comp():
+    resume_words = []
+    job_words = []
+    missing_words = []
+    omit_words = ['and', 'to', 'of', 'in', 'the', 'with', 'or', 'and/or', 'i.e.', 'etc.', 'as', 'by', 'for', 'on', 'not', 'but', 'is']
+    job_count = request.args.get('job_count')
+    job_count = job_count.replace("':",' ')
+    job_count = job_count.replace('":',' ')
+    job_count = job_count.replace(", '",' ')
+    job_count = job_count.replace(', "',' ')
+    job_count = job_count.replace("{'",'')
+    job_count = job_count.replace("}",'')
+    job_count = job_count.split(' ')
+    for word in job_count:
+        if len(word) <= 1:
+            pass
+        else:
+            job_words.append(word)
+    resume_count = request.args.get('resume')
+    resume_count = resume_count.replace('\n',' ')
+    resume_count = resume_count.replace('/',' ')
+    resume_count = resume_count.replace(',','')
+    resume_count = resume_count.split(' ')
+    for words in resume_count:
+        words = words.lower()
+        words = words.strip()
+        if len(words) <= 1:
+            pass
+        elif words in omit_words:
+            pass
+        else:
+            resume_words.append(words)
+    for values in job_words:
+        if values not in resume_words:
+            missing_words.append(values)
+    for items in missing_words:
+        if items in resume_words:
+            missing_words.remove(items)
+    return render_template('resume-comp.html', missing_words = missing_words)
