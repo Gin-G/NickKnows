e3b0d5c77878d1ce774b50bb94a1464b12026286
Nick Cote <ncote@ucar.edu>
2024-07-01 10:39:46 -0600

    new values for testing

--- nickknows/__init__.py
+++ nickknows/__init__.py
@@ -3,8 +3,8 @@ from flask.templating import render_template
 from celery import Celery
 
 app = Flask(__name__)
-#app.config["CELERY_BROKER_URL"] = 'redis://redis:6379'
-app.config["CELERY_BROKER_URL"] = 'redis://localhost:6379'
+app.config["CELERY_BROKER_URL"] = 'redis://redis:6379'
+#app.config["CELERY_BROKER_URL"] = 'redis://localhost:6379'
 app.config["SECRET_KEY"] = "celery blooody nick ski movie music know nfl"
 celery = Celery(app.name, broker=app.config["CELERY_BROKER_URL"])
 celery.conf.update(app.config)
