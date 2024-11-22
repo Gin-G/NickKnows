45e73f85499561b46cd1ebc2b91d7a0204f2022e
Nick <ncote@ucar.edu>
2024-11-22 12:42:15 -0700

    Add try excepts

--- app/nickknows/celery_setup/tasks.py
+++ app/nickknows/celery_setup/tasks.py
@@ -51,15 +51,19 @@ def update_qb_yards_top10():
     else:
         update_roster_data.delay()
         #flash('Data is updating in the background. Refresh the page in a bit')
-    pbp_data = pbp_data[pbp_data["season_type"] == "REG"]
-    pbp_data = pbp_data[pbp_data["two_point_attempt"] == False]
-    pbp_data_pass = pbp_data[pbp_data["play_type"] == "pass"]
-    pbp_data_pass = pbp_data_pass.merge(roster_data[["player_id","player_name"]], left_on="passer_player_id", right_on="player_id")
-    pass_agg = pbp_data_pass.groupby(["player_name"], as_index=False).agg({"passing_yards": "sum"})
-    pass_agg.sort_values(by=['passing_yards'], inplace=True, ascending=False)
-    pass_agg.rename(columns={'player_name':'Player Name','passing_yards':"Total Passing Yards"}, inplace=True)
-    pass_agg = pass_agg.head(10)
-    pass_agg.to_csv(qb10file_path)
+    try:
+        pbp_data = pbp_data[pbp_data["season_type"] == "REG"]
+        pbp_data = pbp_data[pbp_data["two_point_attempt"] == False]
+        pbp_data_pass = pbp_data[pbp_data["play_type"] == "pass"]
+        pbp_data_pass = pbp_data_pass.merge(roster_data[["player_id","player_name"]], left_on="passer_player_id", right_on="player_id")
+        pass_agg = pbp_data_pass.groupby(["player_name"], as_index=False).agg({"passing_yards": "sum"})
+        pass_agg.sort_values(by=['passing_yards'], inplace=True, ascending=False)
+        pass_agg.rename(columns={'player_name':'Player Name','passing_yards':"Total Passing Yards"}, inplace=True)
+        pass_agg = pass_agg.head(10)
+        pass_agg.to_csv(qb10file_path)
+    except:
+        update_PBP_data.delay()
+        update_roster_data.delay()
 
 @celery.task()
 def update_qb_tds_top10():
@@ -76,15 +80,19 @@ def update_qb_tds_top10():
     else:
         update_roster_data.delay()
         #flash('Data is updating in the background. Refresh the page in a bit')
-    pbp_data = pbp_data[pbp_data["season_type"] == "REG"]
-    pbp_data = pbp_data[pbp_data["two_point_attempt"] == False]
-    pbp_data_pass = pbp_data[pbp_data["play_type"] == "pass"]
-    pbp_data_pass = pbp_data_pass.merge(roster_data[["player_id","player_name"]], left_on="passer_player_id", right_on="player_id")
-    pass_td_agg = pbp_data_pass.groupby(["player_name"], as_index=False).agg({"pass_touchdown":"sum"})
-    pass_td_agg.sort_values(by=['pass_touchdown'], inplace=True, ascending=False)
-    pass_td_agg.rename(columns={'player_name':'Player Name',"pass_touchdown":"Total Passing TD's"}, inplace=True)
-    pass_td_agg = pass_td_agg.head(10)
-    pass_td_agg.to_csv(qbtd10file_path)
+    try:
+        pbp_data = pbp_data[pbp_data["season_type"] == "REG"]
+        pbp_data = pbp_data[pbp_data["two_point_attempt"] == False]
+        pbp_data_pass = pbp_data[pbp_data["play_type"] == "pass"]
+        pbp_data_pass = pbp_data_pass.merge(roster_data[["player_id","player_name"]], left_on="passer_player_id", right_on="player_id")
+        pass_td_agg = pbp_data_pass.groupby(["player_name"], as_index=False).agg({"pass_touchdown":"sum"})
+        pass_td_agg.sort_values(by=['pass_touchdown'], inplace=True, ascending=False)
+        pass_td_agg.rename(columns={'player_name':'Player Name',"pass_touchdown":"Total Passing TD's"}, inplace=True)
+        pass_td_agg = pass_td_agg.head(10)
+        pass_td_agg.to_csv(qbtd10file_path)
+    except:
+        update_PBP_data.delay()
+        update_roster_data.delay()
 
 @celery.task()
 def update_rb_yards_top10():
@@ -101,15 +109,19 @@ def update_rb_yards_top10():
     else:
         update_roster_data.delay()
         #flash('Data is updating in the background. Refresh the page in a bit')
-    pbp_data = pbp_data[pbp_data["season_type"] == "REG"]
-    pbp_data = pbp_data[pbp_data["two_point_attempt"] == False]
-    pbp_data_rush = pbp_data[pbp_data["play_type"] == "run"]
-    pbp_data_rush = pbp_data_rush.merge(roster_data[["player_id","player_name"]], left_on="rusher_player_id", right_on="player_id")
-    rush_yds_agg = pbp_data_rush.groupby(["player_name"], as_index=False).agg({"rushing_yards": "sum"})
-    rush_yds_agg.sort_values(by=['rushing_yards'], inplace=True, ascending=False)
-    rush_yds_agg.rename(columns={'player_name':'Player Name','rushing_yards':"Total Rushing Yards"}, inplace=True)
-    rush_yds_agg = rush_yds_agg.head(10)
-    rush_yds_agg.to_csv(rbyds10)
+    try:
+        pbp_data = pbp_data[pbp_data["season_type"] == "REG"]
+        pbp_data = pbp_data[pbp_data["two_point_attempt"] == False]
+        pbp_data_rush = pbp_data[pbp_data["play_type"] == "run"]
+        pbp_data_rush = pbp_data_rush.merge(roster_data[["player_id","player_name"]], left_on="rusher_player_id", right_on="player_id")
+        rush_yds_agg = pbp_data_rush.groupby(["player_name"], as_index=False).agg({"rushing_yards": "sum"})
+        rush_yds_agg.sort_values(by=['rushing_yards'], inplace=True, ascending=False)
+        rush_yds_agg.rename(columns={'player_name':'Player Name','rushing_yards':"Total Rushing Yards"}, inplace=True)
+        rush_yds_agg = rush_yds_agg.head(10)
+        rush_yds_agg.to_csv(rbyds10)
+    except:
+        update_PBP_data.delay()
+        update_roster_data.delay()
 
 @celery.task()
 def update_rb_tds_top10():
@@ -126,15 +138,19 @@ def update_rb_tds_top10():
     else:
         update_roster_data.delay()
         #flash('Data is updating in the background. Refresh the page in a bit')
-    pbp_data = pbp_data[pbp_data["season_type"] == "REG"]
-    pbp_data = pbp_data[pbp_data["two_point_attempt"] == False]
-    pbp_data_rush = pbp_data[pbp_data["play_type"] == "run"]
-    pbp_data_rush = pbp_data_rush.merge(roster_data[["player_id","player_name"]], left_on="rusher_player_id", right_on="player_id")
-    rush_td_agg = pbp_data_rush.groupby(["player_name"], as_index=False).agg({"rush_touchdown":"sum"})
-    rush_td_agg.sort_values(by=['rush_touchdown'], inplace=True, ascending=False)
-    rush_td_agg.rename(columns={'player_name':'Player Name',"rush_touchdown":"Total Rushing TD's"}, inplace=True)
-    rush_td_agg = rush_td_agg.head(10)
-    rush_td_agg.to_csv(rbtds10)
+    try:
+        pbp_data = pbp_data[pbp_data["season_type"] == "REG"]
+        pbp_data = pbp_data[pbp_data["two_point_attempt"] == False]
+        pbp_data_rush = pbp_data[pbp_data["play_type"] == "run"]
+        pbp_data_rush = pbp_data_rush.merge(roster_data[["player_id","player_name"]], left_on="rusher_player_id", right_on="player_id")
+        rush_td_agg = pbp_data_rush.groupby(["player_name"], as_index=False).agg({"rush_touchdown":"sum"})
+        rush_td_agg.sort_values(by=['rush_touchdown'], inplace=True, ascending=False)
+        rush_td_agg.rename(columns={'player_name':'Player Name',"rush_touchdown":"Total Rushing TD's"}, inplace=True)
+        rush_td_agg = rush_td_agg.head(10)
+        rush_td_agg.to_csv(rbtds10)
+    except:
+        update_PBP_data.delay()
+        update_roster_data.delay()
 
 @celery.task()
 def update_rec_yds_top10():
@@ -151,15 +167,19 @@ def update_rec_yds_top10():
     else:
         update_roster_data.delay()
         #flash('Data is updating in the background. Refresh the page in a bit')
-    pbp_data = pbp_data[pbp_data["season_type"] == "REG"]
-    pbp_data = pbp_data[pbp_data["two_point_attempt"] == False]
-    pbp_data_rec = pbp_data[pbp_data["play_type"] == "pass"]
-    pbp_data_rec = pbp_data_rec.merge(roster_data[["player_id","player_name"]], left_on="receiver_player_id", right_on="player_id")
-    rec_yds_agg = pbp_data_rec.groupby(["player_name"], as_index=False).agg({"receiving_yards": "sum"})
-    rec_yds_agg.sort_values(by=['receiving_yards'], inplace=True, ascending=False)
-    rec_yds_agg.rename(columns={'player_name':'Player Name','receiving_yards':"Total Receiving Yards"}, inplace=True)
-    rec_yds_agg = rec_yds_agg.head(10)
-    rec_yds_agg.to_csv(recyds10)
+    try:
+        pbp_data = pbp_data[pbp_data["season_type"] == "REG"]
+        pbp_data = pbp_data[pbp_data["two_point_attempt"] == False]
+        pbp_data_rec = pbp_data[pbp_data["play_type"] == "pass"]
+        pbp_data_rec = pbp_data_rec.merge(roster_data[["player_id","player_name"]], left_on="receiver_player_id", right_on="player_id")
+        rec_yds_agg = pbp_data_rec.groupby(["player_name"], as_index=False).agg({"receiving_yards": "sum"})
+        rec_yds_agg.sort_values(by=['receiving_yards'], inplace=True, ascending=False)
+        rec_yds_agg.rename(columns={'player_name':'Player Name','receiving_yards':"Total Receiving Yards"}, inplace=True)
+        rec_yds_agg = rec_yds_agg.head(10)
+        rec_yds_agg.to_csv(recyds10)
+    except:
+        update_PBP_data.delay()
+        update_roster_data.delay()
 
 @celery.task()
 def update_rec_tds_top10():
@@ -176,16 +196,20 @@ def update_rec_tds_top10():
     else:
         update_roster_data.delay()
         #flash('Data is updating in the background. Refresh the page in a bit')
-    pbp_data = pbp_data[pbp_data["season_type"] == "REG"]
-    pbp_data = pbp_data[pbp_data["two_point_attempt"] == False]
-    pbp_data_rec = pbp_data[pbp_data["play_type"] == "pass"]
-    pbp_data_rec = pbp_data_rec.merge(roster_data[["player_id","player_name"]], left_on="receiver_player_id", right_on="player_id")
-    rec_td_agg = pbp_data_rec.groupby(["player_name"], as_index=False).agg({"pass_touchdown":"sum"})
-    rec_td_agg.sort_values(by=['pass_touchdown'], inplace=True, ascending=False)
-    rec_td_agg.rename(columns={'player_name':'Player Name',"pass_touchdown":"Total Receiving TD's"}, inplace=True)
-    rec_td_agg = rec_td_agg.head(10)
-    rec_td_agg.to_csv(rectds10)
-
+    try:
+        pbp_data = pbp_data[pbp_data["season_type"] == "REG"]
+        pbp_data = pbp_data[pbp_data["two_point_attempt"] == False]
+        pbp_data_rec = pbp_data[pbp_data["play_type"] == "pass"]
+        pbp_data_rec = pbp_data_rec.merge(roster_data[["player_id","player_name"]], left_on="receiver_player_id", right_on="player_id")
+        rec_td_agg = pbp_data_rec.groupby(["player_name"], as_index=False).agg({"pass_touchdown":"sum"})
+        rec_td_agg.sort_values(by=['pass_touchdown'], inplace=True, ascending=False)
+        rec_td_agg.rename(columns={'player_name':'Player Name',"pass_touchdown":"Total Receiving TD's"}, inplace=True)
+        rec_td_agg = rec_td_agg.head(10)
+        rec_td_agg.to_csv(rectds10)
+    except:
+        update_PBP_data.delay()
+        update_roster_data.delay()
+        
 @celery.task()
 def update_team_stats(team):
     # Open Schedule data
